0884ed3e0545aaf58bc8b8d17c19c2b2833e78e5
Samuele Musiani <samu@teapot.ovh>
2025-01-22 11:36:43 +0100

    radioStatus, ssidList

--- status.go
+++ status.go
@@ -120,3 +120,98 @@ func (s *Session) WifiClientNum() (*WifiClientNum, error) {
 
 	return &wc.WifiClientNum, nil
 }
+
+type RadioType string
+
+var Radio2_4G RadioType = "2.4G"
+var Radio5G RadioType = "5G"
+
+type WifiRadioStatusRequest struct {
+	Radio RadioType `json:"radio"`
+}
+
+type WifiRadioStatusRequestWrap struct {
+	WifiRadioStatus WifiRadioStatusRequest `json:"wifiRadioStatus"`
+}
+
+type WifiRadioStatusResponse struct {
+	NetMode     string `json:"netMode"`
+	Channel     string `json:"channel"`
+	WifiRadioEn bool   `json:"wifiRadioEn"`
+}
+
+type WifiRadioStatusResponseWrap struct {
+	WifiRadioStatus WifiRadioStatusResponse `json:"wifiRadioStatus"`
+}
+
+func (s *Session) WifiRadioStatus(radio RadioType) (*WifiRadioStatusResponse, error) {
+	rbody, err := json.Marshal(WifiRadioStatusRequestWrap{WifiRadioStatusRequest{Radio: radio}})
+	req, err := http.NewRequest("POST", s.uri+MODULES_PATH, bytes.NewReader(rbody))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range s.cookies {
+		req.AddCookie(c)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var wr WifiRadioStatusResponseWrap
+	err = json.NewDecoder(resp.Body).Decode(&wr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &wr.WifiRadioStatus, nil
+}
+
+type WifiSSIDListRequestWrap struct {
+	WifiSSIDList WifiSSIDListRequest `json:"wifiSsidList"`
+}
+
+type WifiSSIDListRequest struct {
+	Radio RadioType `json:"radio"`
+}
+
+type WifiSSIDListResponseWrap struct {
+	WifiSSIDList WifiSSIDListResponse `json:"wifiSsidList"`
+}
+
+type WifiSSIDListResponse []SSID
+
+type SSID struct {
+	SSID     string `json:"ssid"`
+	Enabled  bool   `json:"wifiSsidEn"`
+	Mac      string `json:"mac"`
+	Security string `json:"security"`
+}
+
+func (s *Session) WifiSSIDList(radio RadioType) (*WifiSSIDListResponse, error) {
+	rbody, err := json.Marshal(WifiSSIDListRequestWrap{WifiSSIDListRequest{Radio: radio}})
+
+	req, err := http.NewRequest("POST", s.uri+MODULES_PATH, bytes.NewReader(rbody))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range s.cookies {
+		req.AddCookie(c)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var ws WifiSSIDListResponseWrap
+	err = json.NewDecoder(resp.Body).Decode(&ws)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ws.WifiSSIDList, nil
+}
