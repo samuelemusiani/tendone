fac6b3b4cd9f1bb960c9a9965bc7e6ae1d61e978
Samuele Musiani <samu@teapot.ovh>
2025-01-22 12:55:30 +0100

    add: lan get and set

---
+++ lan.go
@@ -0,0 +1,91 @@
+package tendone
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+type LanConfigGetRequestWrap struct {
+	Config LanConfigGetRequest `json:"lanCfgGet"`
+}
+
+type LanConfigGetRequest struct{}
+
+type LanConfigGetResponseWrap struct {
+	Config LanConfig `json:"lanCfgGet"`
+}
+
+type LanConfig struct {
+	Type       string `json:"lanType"`
+	DeviceName string `json:"deviceName"`
+	MAC        string `json:"lanMac"`
+	IP         string `json:"lanIp"`
+	Mask       string `json:"lanMask"`
+	Gateway    string `json:"lanGw"`
+	DNS0       string `json:"preDns"`
+	DNS1       string `json:"altDns"`
+	EthMode    string `json:"ethMode"`
+}
+
+type LanConfigSetRequestWrap struct {
+	Config LanConfig `json:"lanCfgSet"`
+}
+
+type LanConfigSetResponseWrap struct {
+	Resp string `json:"lanCfgSet"`
+}
+
+func (s *Session) LanConfigGet() (*LanConfig, error) {
+	rbody, err := json.Marshal(LanConfigGetRequestWrap{})
+	req, err := http.NewRequest("POST", s.uri+MODULES_PATH, bytes.NewReader(rbody))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range s.cookies {
+		req.AddCookie(c)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var lcr LanConfigGetResponseWrap
+	err = json.NewDecoder(resp.Body).Decode(&lcr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &lcr.Config, nil
+}
+
+func (s *Session) LanConfigSet(lc *LanConfig) (bool, error) {
+	rbody, err := json.Marshal(LanConfigSetRequestWrap{Config: *lc})
+	req, err := http.NewRequest("POST", s.uri+MODULES_PATH, bytes.NewReader(rbody))
+	if err != nil {
+		return false, err
+	}
+
+	for _, c := range s.cookies {
+		req.AddCookie(c)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+
+	var lcs LanConfigSetResponseWrap
+	err = json.NewDecoder(resp.Body).Decode(&lcs)
+	if err != nil {
+		return false, err
+	}
+
+	if lcs.Resp == "ok" {
+		return true, nil
+	}
+
+	return false, nil
+}
