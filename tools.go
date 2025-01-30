da8b02ca88ef0ff60f75fdfba4a9302afe8515af
Samuele Musiani <samu@teapot.ovh>
2025-01-30 18:59:54 +0100

    add: tools backup download

--- tools.go
+++ tools.go
@@ -1,6 +1,11 @@
 package tendone
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+)
 
 type rebootRequestWrap struct {
 	RebootRequest RebootRequest `json:"sysReboot"`
@@ -18,5 +23,39 @@ func (s *Session) Reboot() error {
 
 	// No Response for the reboot command
 	_, err = fetch(s, rbody)
+	return err
+}
+
+func (s *Session) Backup(path string) error {
+	urlPath := "/cgi-bin/DownloadCfg/APCfm.cfg"
+
+	if _, err := os.Stat(path); err == nil {
+		return os.ErrExist
+	} else if !os.IsNotExist(err) {
+		return err
+	}
+
+	req, err := http.NewRequest("GET", s.uri+urlPath, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, c := range s.cookies {
+		req.AddCookie(c)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
 	return err
 }
